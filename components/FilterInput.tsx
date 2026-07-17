90c65b44649369dd270a11efae3b8feacf740361
Getahun Alemayhu <getahune.alemayhu@gmail.com>
2026-07-17 11:17:49 +0300

    filter based on the months Show only months with sales above this value

--- components/FilterInput.tsx
+++ components/FilterInput.tsx
@@ -1,20 +1,26 @@
 "use client";
 
 interface FilterInputProps {
+  value: number;
   onThresholdChange: (value: number) => void;
 }
 
-export default function FilterInput({ onThresholdChange }: FilterInputProps) {
+export default function FilterInput({
+  value,
+  onThresholdChange,
+}: FilterInputProps) {
   return (
     <div>
       <label className="label-text">Sales Threshold</label>
       <input
         type="number"
         min="0"
-        placeholder="Enter threshold"
+        value={value || ""}
+        placeholder="e.g. 400000"
         onChange={(event) => onThresholdChange(Number(event.target.value))}
         className="input-field"
       />
+      <p className="field-hint">Show only months with sales above this value.</p>
     </div>
   );
 }
